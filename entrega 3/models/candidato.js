254468cd93e61a6ae37170e46be07dd14ba342ba
vicente <108952417+xxvnte@users.noreply.github.com>
2025-03-09 13:12:59 -0300

    Delete entrega 3 directory

--- entrega 3/models/candidato.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const CandidatoSchema = new mongoose.Schema({
-  nombre: { type: String, required: true }
-});
-
-const Candidato = mongoose.model('Candidato', CandidatoSchema);
-
-module.exports = Candidato;
